7af80788e31409369ab50805c26533b750eb22de
Ludwig Breitsprecher <ludwig.breitsprecher@tu-ilmenau.de>
2016-08-03 00:33:59 +0200

    remove dev-tools, install postgres, composer

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 this repository delivers a vagrant enviroment with the following software:
 - ubuntu 14.04
-- nginx
-- php 7.09
+- nginx             
+- php 7.09          
 - phalcon 3.0
-- phalcon developer tools 3.0
+- postgresql        
+
+
+forwarded ports (can be changed in vagrantfile):
+- 80 - nginx
+- 9000 - php-fpm
+- 5432 - postgres
--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo -i
-
 #
 # Add PHP and PostgreSQL and Nginx repositories
 #
@@ -24,39 +22,49 @@ sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 sudo apt-key update
 sudo apt-get update -qq
 sudo apt-get upgrade -y --force-yes
-sudo apt-get install -y build-essential software-properties-common python-software-properties
 sudo apt-get autoremove -y
 
 #
 # install basics
 #
 sudo apt-get install -y \
-  curl \
-  git \
-  unzip \
-  grc \
-  gcc \
-  make \
-  re2c \
-  libpcre3-dev
+    curl \
+    git \
+    unzip \
+    grc \
+    gcc \
+    make \
+    re2c \
+    libpcre3-dev \
+    build-essential \
+    software-properties-common \
+    python-software-properties
 
 #
 # install php
 #
 sudo apt-get install -y \
-php7.0 \
-php7.0-fpm \
-php7.0-dev
+    php7.0 \
+    php7.0-fpm \
+    php7.0-cli \
+    php7.0-curl \
+    php7.0-gd \
+    php7.0-intl \
+    php7.0-zip \
+    php7.0-pgsql \
+    php7.0-dev
 
 #
-# install nginx
+# install nginx, postgres
 #
-sudo apt-get install nginx
+sudo apt-get install -y \
+    nginx \
+    postgresql \
+    postgresql-contrib
 
 #
 # install phalcon
 #
-
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
 sudo ./install
@@ -64,16 +72,11 @@ sudo touch /etc/php/7.0/fpm/conf.d/30-phalcon.ini
 sudo echo "extension=phalcon.so" > /etc/php/7.0/fpm/conf.d/30-phalcon.ini
 sudo touch /etc/php/7.0/cli/conf.d/30-phalcon.ini
 sudo echo "extension=phalcon.so" > /etc/php/7.0/cli/conf.d/30-phalcon.ini
-cd ../..
 
 #
 # Composer for PHP
 #
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-#
-# install devtools
-#
 
 #sudo git clone git://github.com/phalcon/phalcon-devtools.git
 #cd phalcon-devtools
